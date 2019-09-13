196c7adbd0ddd24144231d39b735afed9aaeae26
root <root@ComputerName.localdomain>
2019-09-13 23:06:37 +0900

    change

--- hangman.py
+++ hangman.py
@@ -1,9 +1,5 @@
 def hangman(word):
     wrong = 0
-    result = list(word)
-    youser_input = input("文字を入力してください。")
-    ans = list(youser_input)
-    print(ans)
     stages = ["",
               "____   ",
 
@@ -13,13 +9,38 @@ def hangman(word):
 
               "|   0   ",
 
-              "| / |  :    ",
+              "| : | :    ",
 
-              "|      ",
+              "|  : :   ",
 
               "|      "
-             
              ]
+    #↓正解の文字を一文字づつ分割する↓
+    rletters = list(word)
+    board = "_" * len(word)
+    win = False
+    print("welcometohangman")
+    while wrong < len(stages) -1:
+        print("\n")
+        msg = "Expect one character"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
 
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        win = True
+        break
+    if not win :
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け!正解は {}." .format(word))
+    
+    
+        
 
 hangman("cat")
