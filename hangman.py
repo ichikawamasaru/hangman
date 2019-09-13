87d9f0cac1785ef68986ae71eb33e73486a584dd
root <root@ComputerName.localdomain>
2019-09-13 15:44:36 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,25 @@
+def hangman(word):
+    wrong = 0
+    result = list(word)
+    youser_input = input("文字を入力してください。")
+    ans = list(youser_input)
+    print(ans)
+    stages = ["",
+              "____   ",
+
+              "|      ",
+              
+              "|   |   ",
+
+              "|   0   ",
+
+              "| / |      ",
+
+              "|      ",
+
+              "|      "
+             
+             ]
+
+
+hangman("cat")
